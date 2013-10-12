155be40b37b686bb9a7dc07bb385e86628f5530e
yuttadhammo <yuttadhammo@gmail.com>
2013-10-11 21:38:20 -0500

    added auto toggle on click index

--- src/org/yuttadhammo/tipitaka/ReadBookActivity.java
+++ src/org/yuttadhammo/tipitaka/ReadBookActivity.java
@@ -294,6 +294,7 @@ public class ReadBookActivity extends SherlockFragmentActivity {
 				public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
 					lastPosition = position;
 					mPager.setCurrentItem(lastPosition);
+					slideMenu.toggle();
 			  	}
 			});
 
